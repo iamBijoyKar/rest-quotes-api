559a0779f5c27964371e58d5e5238fc86b67ea6a
iamBijoyKar <bijoypoco12345@gmail.com>
2022-12-14 18:04:11 +0530

    quotes increased

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from flask import Flask,jsonify
+from quote import quoteGenerate
 
-import random
-
-quotes = [
-  "If you want to be happy, be. - Leo Tolstoy",
-  "It is not enough to be busy. So are the ants. The question is: What are we busy about? - Henry David Thoreau",
-  "The only way to do great work is to love what you do. - Steve Jobs"
-]
 
 app = Flask(__name__)
 
 @app.route('/api/quotes/random')
 def generate_random_quote():
-    quote = random.choice(quotes)
+    quote = quoteGenerate()
     return jsonify(quote)
 
 if __name__ == '__main__':
