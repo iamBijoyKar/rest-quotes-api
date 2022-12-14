3bbbd9b6562f6bef892ed48feb7af8999edb2f2b
iamBijoyKar <bijoypoco12345@gmail.com>
2022-12-14 18:17:07 +0530

    error resolve

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask,jsonify
-from quote import quoteGenerate
+import json
+import random
 
+def quoteGenerate():
+  with open('quotes.json','r') as f:
+    data = json.load(f)
+    quote = random.choice(data)
+    print(quote)
+    return quote
 
 app = Flask(__name__)
 
 @app.route('/api/quotes/random')
 def generate_random_quote():
-    quote = quoteGenerate()
-    return jsonify(quote)
+  quote = quoteGenerate()
+  return jsonify(quote)
 
 if __name__ == '__main__':
   app.run()
